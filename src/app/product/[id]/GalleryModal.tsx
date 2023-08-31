df9b84e9d1355facdf4eead9e8e953bb8f756f0e
E1-XP <poczta.elektroniczna@hotmail.com>
2023-08-31 15:27:03 +0200

    add gallery zoom on mouse over

---
+++ src/app/product/[id]/GalleryModal.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+const GalleryModal = () => {
+  return <div className="">ok</div>;
+};
+
+export default GalleryModal;
--- src/app/product/[id]/ProductGallery.tsx
+++ src/app/product/[id]/ProductGallery.tsx
@@ -10,6 +10,23 @@ interface Props {
 
 const ProductGallery = ({ data }: Props) => {
   const [activeIdx, setActiveIdx] = React.useState(0);
+  const [coords, setCoords] = React.useState({ x: 50, y: 50 });
+  const [isHovering, setIsHovering] = React.useState(false);
+
+  const galleryWrapper = React.useRef<HTMLDivElement | null>(null);
+
+  const onMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (!galleryWrapper.current) return;
+
+    const rect = galleryWrapper.current?.getBoundingClientRect();
+    const yPos = Math.max(0, Math.floor(e.pageY - rect.top));
+    const xPos = Math.max(0, Math.floor(e.pageX - rect.left));
+
+    const x = (xPos / rect.width) * 100;
+    const y = (yPos / rect.height) * 100;
+
+    setCoords({ x, y });
+  };
 
   return (
     <div className="flex flex-col md:flex-row gap-4">
@@ -33,13 +50,24 @@ const ProductGallery = ({ data }: Props) => {
           </button>
         ))}
       </div>
-      <div className="basis-5/6 max-md:-order-1">
+      <div
+        className="basis-5/6 max-md:-order-1 overflow-hidden group cursor-zoom-in"
+        onMouseMove={onMouseMove}
+        onMouseEnter={() => setIsHovering(true)}
+        onMouseLeave={() => setIsHovering(false)}
+        ref={galleryWrapper}
+      >
         <Image
           src={data.images[activeIdx]}
           alt={data.name}
           width={1800}
           height={2600}
-          className="pointer-events-none"
+          className="pointer-events-none group-hover:object-none"
+          style={{
+            objectPosition: isHovering
+              ? `${coords.x}% ${coords.y}%`
+              : undefined,
+          }}
         />
       </div>
     </div>
