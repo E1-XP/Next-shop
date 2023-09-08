c427855f019ae4311cb85680430dc8e4cce096c3
E1-XP <poczta.elektroniczna@hotmail.com>
2023-09-08 22:26:51 +0200

    fix: adjust header for commonly used screensizes

--- src/app/(home)/Hero.tsx
+++ src/app/(home)/Hero.tsx
@@ -21,6 +21,7 @@ import ArrowRightIcon from "../components/icons/ArrowRight";
 const HeroSection = () => {
   const data = {
     heading: "Featured",
+    buttonText: "See collection",
     slides: [
       {
         img: slide1,
@@ -32,14 +33,14 @@ const HeroSection = () => {
       {
         img: slide2,
         imgColor: "#6b8d95",
-        heading: "Sizzle in Style: Hot Summer Women's Fashion",
+        heading: "Stay Cool, Look Hot: Summer Styles on Sale!",
         paragraph:
           "Make a statement this summer with our bold and vibrant new collection for women - perfect for turning heads and standing out in style.",
       },
       {
         img: slide3,
         imgColor: "#d02e6e",
-        heading: "Sizzle in Style: Hot Summer Women's Fashion",
+        heading: "Season Sale! Up to 70% down!",
         paragraph:
           "Make a statement this summer with our bold and vibrant new collection for women - perfect for turning heads and standing out in style.",
       },
@@ -56,7 +57,7 @@ const HeroSection = () => {
 
   return (
     <section
-      className="sm:h-[77vh] -mt-[72px] pt-[72px] transition-colors delay-200"
+      className="sm:h-[77vh] max-xl:min-h-[65vw] max-h-[750px] -mt-[72px] pt-[72px] transition-colors delay-200"
       style={{
         background: `linear-gradient(289deg, transparent 20%, rgba(33, 33, 33, 0.95) 100%), ${data.slides[slideIdx].imgColor}`,
       }}
@@ -83,12 +84,12 @@ const HeroSection = () => {
               <Image
                 src={item.img}
                 alt="Product campaign photo"
-                className={`pointer-events-none w-full h-full object-cover object-center md:object-[0%_48%] mask-image max-sm:!basis-1/2 ${
+                className={`pointer-events-none w-full h-full object-cover object-center md:object-[10%_48%] mask-image max-sm:!basis-1/2 ${
                   i === 2 ? "max-sm:object-center max-md:object-left" : ""
                 }`}
               />
-              <div className="max-sm:wrapper max-sm:h-[305px] max-[400px]:h-[360px] max-sm:mb-10 max-sm:justify-center sm:absolute top-[50%] sm:top-[10%] md:top-[12%] inset-4 sm:left-10 lg:left-14 sm:w-[400px] lg:w-[545px] flex flex-col items-start gap-4">
-                <p className="heading-3 sm:heading-2 lg:heading-1 uppercase !text-white">
+              <div className="max-sm:wrapper max-sm:h-[305px] max-[400px]:h-[320px] max-sm:mb-10 max-sm:justify-center sm:absolute top-[50%] sm:top-[10%] md:top-[12%] inset-4 sm:left-10 lg:left-14 sm:w-[400px] lg:w-[545px] flex flex-col items-start gap-4">
+                <p className="heading-3 max-sm:text-[32px] max-sm:leading-9 sm:heading-2 lg:heading-1 uppercase !text-white">
                   {item.heading}
                 </p>
                 <p className="text text-white">{item.paragraph}</p>
@@ -96,7 +97,7 @@ const HeroSection = () => {
                   alt={true}
                   className="flex items-center gap-2 group mt-3"
                 >
-                  See collection
+                  {data.buttonText}
                   <ArrowRightIcon className="transition group-hover:translate-x-1" />
                 </Button>
               </div>
