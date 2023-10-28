926ce6e876f99ee58fc3d3786a79ded04f0da17f
E1-XP <poczta.elektroniczna@hotmail.com>
2023-10-28 18:16:01 +0200

    fix: use envs to construct urls in app pt3

--- src/app/_trpc/serverClient.ts
+++ src/app/_trpc/serverClient.ts
@@ -5,7 +5,7 @@ import { appRouter } from "@/server/routers";
 export const serverClient = appRouter.createCaller({
   links: [
     httpBatchLink({
-      url: `${process.env.BASE_URL}/api/trpc`,
+      url: "/api/trpc",
     }),
   ],
 });
